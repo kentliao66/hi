ae94e8be9b43b14272ae2c207336e2859f5dda89
Kent Liao <kentliao66@gmail.com>
2020-06-09 11:55:40 +0800

    Add New File

---
+++ 123.py
@@ -0,0 +1,17 @@
+name = input('Please Input the Name: ')
+x=5
+print(x)
+x=10
+y=2
+y=1000
+print(y+x)
+price = 10
+pen = 0.38
+rain  = True
+print (price, pen, rain, 'Hi', name)
+print (pen)
+print (rain)
+print (name)
+hight = input ('Please Input your Hight')
+weight = input ('Please Input your weight')
+print ('My Hight is ', hight, ', My Weight is ', weight)
